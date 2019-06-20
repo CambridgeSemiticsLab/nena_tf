34b5a6597920f28e2133c382e1c3a1e8af50a7ab
Cody Kingham <codyakingham@gmail.com>
2019-06-20 11:40:48 +0100

    Add utf8 and trailer features to words and morphemes

--- tf/otext.tf
+++ tf/otext.tf
@@ -4,5 +4,5 @@
 @sectionFeatures=text_id,line
 @sectionTypes=text,line
 @writtenBy=Text-Fabric
-@dateWritten=2019-06-20T10:13:11Z
+@dateWritten=2019-06-20T10:37:19Z
 
--- tf/paragraph.tf
+++ tf/paragraph.tf
@@ -2,5 +2,5 @@
 @author=Geoffrey Khan, Cody Kingham, and Hannes Vlaardingerbroek
 @valueType=str
 @writtenBy=Text-Fabric
-@dateWritten=2019-06-20T10:13:11Z
+@dateWritten=2019-06-20T10:37:19Z
 
